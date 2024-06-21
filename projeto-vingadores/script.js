0f2a486e81617d4afb2e1adc4813d785f6517474
Victor <169072858+victordamasodev@users.noreply.github.com>
2024-06-21 12:01:02 -0300

    projeto vingadores

---
+++ projeto-vingadores/script.js
@@ -0,0 +1,30 @@
+function active(opcao){
+    let option1 = document.getElementById('opcao1');
+    let option2 = document.getElementById('opcao2');
+    let option3 = document.getElementById('opcao3');
+
+    let banner = document.getElementById('banner')
+    
+    if(opcao== 1){
+        option1.style.backgroundColor = '#f13239';
+        option2.style.backgroundColor = 'white';
+        option3.style.backgroundColor = 'white';
+
+        banner.style.backgroundImage =  'url(images/banner.jpg)'
+
+    } else if (opcao == 2){
+        option2.style.backgroundColor = '#f13239';
+        option1.style.backgroundColor = 'white';
+        option3.style.backgroundColor = 'white';
+
+        banner.style.backgroundImage =  'none'
+        banner.style.backgroundColor =  'green'
+    } else if (opcao ==3){
+        option1.style.backgroundColor = 'white';
+        option2.style.backgroundColor = 'white';
+        option3.style.backgroundColor = '#f13239';
+        
+        banner.style.backgroundImage =  'none'
+        banner.style.backgroundColor =  'orange'
+    }
+}
